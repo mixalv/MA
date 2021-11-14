89925867b8a33b2c776be948e11ccdd3bda5544f
Mykhailo Khomenko <mixalv@gmail.com>
2021-11-14 21:48:07 +0200

    task 2 solution

---
+++ Homework_3/2.rb
@@ -0,0 +1,62 @@
+# 1)
+class Brackets
+  def initialize(string)
+    @string = string
+  end
+  def valid?
+    stack = []
+    open_parentheses = ['{', '[', '(', '<']
+    close_parentheses = ['}', ']', ')', '>']
+    @string.each_char do |char|
+      if open_parentheses.include?(char)
+        stack.unshift(char)
+        next
+      end
+      return false if close_parentheses.include?(char) && stack.empty?
+      if close_parentheses.include?(char) && !stack.empty?
+        if char == '}' && stack[0] == '{'
+          stack.shift
+        elsif char == ']' && stack[0] == '['
+          stack.shift
+        elsif char == ')' && stack[0] == '('
+          stack.shift
+        elsif char == '>' && stack[0] == '<'
+          stack.shift
+        else
+          return false
+        end  
+      end      
+    end
+    return stack.empty? ? true : false
+  end
+end
+# 2)
+class String
+  def valid?
+    stack = []
+    open_parentheses = ['{', '[', '(', '<']
+    close_parentheses = ['}', ']', ')', '>']
+    self.each_char do |char|
+      if open_parentheses.include?(char)
+        stack.unshift(char)
+        next
+      end
+      return false if close_parentheses.include?(char) && stack.empty?
+      if close_parentheses.include?(char) && !stack.empty?
+        if char == '}' && stack[0] == '{'
+          stack.shift
+        elsif char == ']' && stack[0] == '['
+          stack.shift
+        elsif char == ')' && stack[0] == '('
+          stack.shift
+        elsif char == '>' && stack[0] == '<'
+          stack.shift
+        else
+          return false
+        end  
+      end      
+    end
+    return stack.empty? ? true : false
+  end
+end
+
