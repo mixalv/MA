f065885548d23bdf29e5ff19bdea3fde96036ac0
Mykhailo Khomenko <mixalv@gmail.com>
2021-11-28 21:51:35 +0200

    task 1 solution

---
+++ Homework_4/1.rb
@@ -0,0 +1,61 @@
+class BricksFactory
+
+  def initialize
+    @unbroken_bricks = []
+  end
+
+  # creates a given number of bricks
+  def create_bricks(number)
+    number.times do 
+      created_brick = Brick.new(choose_color, generate_serial_number, choose_state)
+      unbroken_bricks << created_brick if created_brick.state == :unbroken
+    end
+  end
+
+  # returns the total number of unbroken bricks for all time
+  def total_number_of_unbroken_bricks
+    unbroken_bricks.count
+  end
+
+  # returns a sorted array of unbroken bricks (sorting by color)
+  def bricks_sorted_by_color
+    unbroken_bricks.sort_by(&:color)
+  end
+
+  # returns the last 10 created unbroken bricks of the requested color.
+  def last_ten_bricks_with_color(color)
+    result = bricks_sorted_by_color.find_all { |elem| elem.color == color}
+    result.count >= 10 ? result.last(10) : 'There is no ten blocks of such color'
+  end
+
+  private
+
+  attr_accessor :unbroken_bricks
+
+  def generate_serial_number
+    total_number_of_unbroken_bricks + 1
+  end
+
+  def choose_state(weight = 80)
+    rand <= weight/100.0 ? :unbroken : :broken
+  end
+
+  def choose_color
+    colors = [:red, :green, :blue, :white, :brown]
+    colors.sample
+  end
+
+end
+
+class Brick
+
+  attr_accessor :color, :serial_number, :state
+
+  def initialize(color, serial_number, state)
+    @color = color
+    @serial_number = serial_number
+    @state = state
+  end
+
+end
+
